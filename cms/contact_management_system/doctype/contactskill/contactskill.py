90d55d4f6deb92171277d427817bffafb10644b4
MohamedKhX <muhamedkhx2@gmail.com>
2024-09-19 14:48:19 +0200

    Add Doctypes

---
+++ cms/contact_management_system/doctype/contactskill/contactskill.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MohamedKhX and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class ContactSkill(Document):
+	pass
---
+++ cms/contact_management_system/doctype/skilllevel/skilllevel.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, MohamedKhX and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("SkillLevel", {
+// 	refresh(frm) {
+
+// 	},
+// });
