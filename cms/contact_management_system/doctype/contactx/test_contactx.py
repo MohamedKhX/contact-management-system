9a158288d9758a13f12298664073f738d7a5546d
MohamedKhX <muhamedkhx2@gmail.com>
2024-09-30 13:40:01 +0200

    Add missing doctypesa

---
+++ cms/contact_management_system/doctype/contactx/test_contactx.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, MohamedKhX and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestContactX(FrappeTestCase):
+	pass
